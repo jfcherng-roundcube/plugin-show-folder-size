6cd67e3c8b50e9852ffd5c6545946d02403dc5dd
Jack Cherng <jfcherng@gmail.com>
2019-06-21 05:18:21 +0800

    Add a single API to get all folder sizes
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,56 +1,46 @@
-global.pluginShowFolderSize = () => {
-  let rcmail = global.rcmail;
+const debug = false;
 
-  let hash_string_to_int = (str) => str.split('').reduce(
-    (sum, char) => ((sum << 5) - sum) + char.charCodeAt(),
-    0
-  );
+const rcmail = global.rcmail;
 
-  let extract_size_from_api_response = (str) => {
-    let size = /(['"])([^'"]+)\1/.exec(str);
+// button onclick function
+global.pluginShowFolderSize = () => rcmail.http_post(
+  'plugin.all-folder-size',
+  {
+    _humanize: 1,
+  },
+  true
+);
 
-    return size ? size[2] : '';
-  };
+let hash_string_to_int = (str) => str.split('').reduce(
+  (sum, char) => ((sum << 5) - sum) + char.charCodeAt(),
+  0
+);
 
-  let html_show_size = (mailbox, size) => {
-    let size_decorated = `(${size})`;
+let html_show_folder_size = (mailbox, size) => {
+  const size_decorated = `(${size})`;
+  const hash_id = 'folder-size-' + Math.abs(hash_string_to_int(mailbox));
 
-    let hash_id = 'folder-size-' + Math.abs(hash_string_to_int(mailbox));
-    let $mailbox_a = $(`#mailboxlist a[rel="${mailbox}"]`);
-    let $size_span = $(`#${hash_id}`, $mailbox_a);
+  let $mailbox_a = $(`#mailboxlist a[rel="${mailbox}"]`);
+  let $size_span = $(`#${hash_id}`, $mailbox_a);
 
-    if ($size_span.length === 0) {
-      $mailbox_a.append(` <span id="${hash_id}">${size_decorated}</span>`);
-    } else {
-      $size_span.html(size_decorated);
-    }
-  };
+  // no previous size has been appended yet, let's create a new one
+  if ($size_span.length === 0) {
+    $mailbox_a.append(` <span id="${hash_id}">${size_decorated}</span>`);
+  }
+  // update previous size
+  else {
+    $size_span.html(size_decorated);
+  }
+};
 
-  let get_folder_size = (mailbox, async = false, callback = () => {}) => $.ajax({
-    type: 'post',
-    url: rcmail.get_task_url('settings') + '&_framed=1&_action=folder-size',
-    data: {
-      _mbox: mailbox,
-      _remote: '1',
-      _unlock: 'loading' + Math.floor(Math.random() * 1e8),
-    },
-    async: Boolean(async),
-    success: callback,
-  });
+let callback_show_folder_size = (resp) => {
+  if (debug) {
+    console.log(resp);
+  }
 
-  // the real working horse
-  $(() => {
-    let mailboxes = rcmail.env.mailboxes_list;
-
-    for (let mailbox of mailboxes) {
-      get_folder_size(
-        mailbox,
-        true,
-        (data) => html_show_size(
-          mailbox,
-          extract_size_from_api_response(JSON.parse(data).exec)
-        )
-      );
-    }
+  $.each(resp, (mailbox, size) => {
+    html_show_folder_size(mailbox, size);
   });
 };
+
+rcmail.addEventListener('plugin.callback_all_folder_size', callback_show_folder_size);
--- show_folder_size.php
+++ show_folder_size.php
@@ -24,13 +24,14 @@ final class show_folder_size extends rcube_plugin
         $action = rcube_utils::get_input_value('_action', rcube_utils::INPUT_GET);
 
         // some pages should not use this plugin
-        if (in_array($action, ['get'])) {
+        if (\in_array($action, ['get'])) {
             return;
         }
 
         $this->load_plugin_config();
 
         $this->add_texts('localization/', true);
+        $this->register_action('plugin.all-folder-size', [$this, 'action_all_folder_size']);
 
         $this->include_stylesheet($this->local_skin_path() . '/' . __CLASS__ . '.css');
         $this->include_script('js/main.min.js');
@@ -43,9 +44,27 @@ final class show_folder_size extends rcube_plugin
     }
 
     /**
+     * The action handler for "plugin.all-folder-size".
+     */
+    public function action_all_folder_size()
+    {
+        $RCMAIL = rcmail::get_instance();
+        $OUTPUT = $RCMAIL->output;
+
+        $humanize = \filter_var(
+            rcube_utils::get_input_value('_humanize', rcube_utils::INPUT_POST),
+            \FILTER_VALIDATE_BOOLEAN
+        );
+        $humanize = isset($humanize) ? $humanize : true;
+
+        $sizes = $this->get_all_folder_size($humanize);
+
+        $OUTPUT->command('plugin.callback_all_folder_size', $sizes);
+        $OUTPUT->send();
+    }
+
+    /**
      * Add a plugin button.
-     *
-     * @return self
      */
     private function add_plugin_button()
     {
@@ -58,8 +77,6 @@ final class show_folder_size extends rcube_plugin
                 'onclick' => 'pluginShowFolderSize();',
             ], 'toolbar');
         }
-
-        return $this;
     }
 
     /**
@@ -67,11 +84,34 @@ final class show_folder_size extends rcube_plugin
      */
     private function load_plugin_config()
     {
-        $rcmail = rcmail::get_instance();
+        $RCMAIL = rcmail::get_instance();
 
         $this->load_config('config.inc.php.dist');
         $this->load_config('config.inc.php');
 
-        $this->config = $rcmail->config;
+        $this->config = $RCMAIL->config;
+    }
+
+    /**
+     * Get size for all folders.
+     *
+     * @param bool $humanize format the result for human reading
+     *
+     * @return array an array in the form of [mailbox_1 => size_1, ...]
+     */
+    private function get_all_folder_size($humanize = false)
+    {
+        $RCMAIL = rcmail::get_instance();
+        $STORAGE = $RCMAIL->storage;
+
+        // somehow the listed folders are duplicated hence unique()
+        $mailboxes = \array_unique($STORAGE->list_folders());
+        $sizes = \array_map([$STORAGE, 'folder_size'], $mailboxes);
+
+        if ($humanize) {
+            $sizes = \array_map([$RCMAIL, 'show_bytes'], $sizes);
+        }
+
+        return \array_combine($mailboxes, $sizes);
     }
 }
