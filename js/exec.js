3c0ffb18c0f9dee0ccda3fc3f9fa91a4d3049d28
Jack Cherng <jfcherng@gmail.com>
2019-07-02 00:20:02 +0800

    Do not fire API request if there is no mailbox list in the UI
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- js/exec.js
+++ js/exec.js
@@ -1,2 +1,11 @@
-// execute the plugin function when DOMs are ready
-global.$(global.plugin_show_folder_size);
+const $ = global.$;
+const plugin_show_folder_size = global.plugin_show_folder_size;
+
+$(() => {
+  // fire API request only when the UI has the mailbox list
+  if (!$('#mailboxlist').length) {
+    return;
+  }
+
+  plugin_show_folder_size();
+});
