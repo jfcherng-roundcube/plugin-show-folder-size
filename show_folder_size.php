1887d1f09c2de8bb39d8298e7161f6637f3f09a9
Jack Cherng <jfcherng@gmail.com>
2020-04-26 02:10:32 +0800

    chore: move locales/ to localization/
    This is how official plugin done.
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- show_folder_size.php
+++ show_folder_size.php
@@ -31,7 +31,7 @@ final class show_folder_size extends rcube_plugin
         // helpful for extended skins, we try to get it's base skin name directly
         $base_skin = $this->get_base_skin_name();
 
-        $this->add_texts('locales', true);
+        $this->add_texts('localization/', false);
         $this->register_action('plugin.folder-size', [$this, 'action_folder_size']);
 
         $this
