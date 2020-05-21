f50a56804e6cc1d353c7a1b0b5f449bd595061b6
Jack Cherng <jfcherng@gmail.com>
2020-05-21 17:23:25 +0800

    refactor: tidy codes
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,23 @@
 const $ = global.$;
 const rcmail = global.rcmail;
 
-const config = rcmail.env['show_folder_size.config'] || {};
-const prefs = rcmail.env['show_folder_size.prefs'] || {};
+const plugin_name = 'show_folder_size';
+const config = rcmail.env[`${plugin_name}.config`] || {};
+const prefs = rcmail.env[`${plugin_name}.prefs`] || {};
 
 const generatePopupContent = (resp) => {
   let html = `
     <table id="show-folder-size-table" class="records-table">
       <thead>
         <tr>
-          <th>${rcmail.gettext('name', 'show_folder_size')}</th>
-          <th>${rcmail.gettext('size', 'show_folder_size')}</th>
+          <th>${rcmail.gettext('name', plugin_name)}</th>
+          <th>${rcmail.gettext('size', plugin_name)}</th>
         </tr>
       </thead>
       <tbody>
   `;
 
-  for (let [id, [size, sizeHumanized]] of Object.entries(resp)) {
+  for (let [id, [size, size_humanized]] of Object.entries(resp)) {
     let mailbox = rcmail.env.mailboxes[id];
     let level = (id.match(/\//g) || []).length;
 
@@ -28,7 +29,7 @@ const generatePopupContent = (resp) => {
             style="margin-left: ${level * 1.5}em"
           >${mailbox.name}</div>
         </td>
-        <td data-size="${size}">${sizeHumanized}</td>
+        <td data-size="${size}">${size_humanized}</td>
       </tr>
     `;
   }
@@ -41,12 +42,12 @@ const generatePopupContent = (resp) => {
 rcmail.addEventListener('init', (evt) => {
   // register the main command
   rcmail.register_command(
-    'plugin.show_folder_size.show-data',
+    `plugin.${plugin_name}.show-data`,
     () => {
       rcmail.http_post(
-        'plugin.show_folder_size.get-folder-size',
+        `plugin.${plugin_name}.get-folder-size`,
         {
-          _callback: 'plugin.show_folder_size.show-data-callback',
+          _callback: `plugin.${plugin_name}.show-data-callback`,
           _folders: rcmail.env.mailboxes_list,
         },
         rcmail.set_busy(true, 'loading')
@@ -56,12 +57,12 @@ rcmail.addEventListener('init', (evt) => {
   );
 
   // the callback function when server-side's API responses.
-  rcmail.addEventListener('plugin.show_folder_size.show-data-callback', (resp) => {
+  rcmail.addEventListener(`plugin.${plugin_name}.show-data-callback`, (resp) => {
     delete resp.event; // unused entry
 
     rcmail.show_popup_dialog(
       generatePopupContent(resp),
-      rcmail.gettext('folder_size', 'show_folder_size')
+      rcmail.gettext('folder_size', plugin_name)
     );
   });
 });
